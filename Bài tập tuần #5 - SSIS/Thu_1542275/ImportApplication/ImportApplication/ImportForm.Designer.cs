e88db0b56835ce9e596149cfe1a97ee526dd473f
Thu Phạm <thupham6951@gmail.com>
2017-04-09 19:16:37 +0700

    Bài tập SSIS - 1542275

---
+++ Bài tập tuần #5 - SSIS/Thu_1542275/ImportApplication/ImportApplication/ImportForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace ImportApplication
+{
+    partial class ImportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPackageURL = new System.Windows.Forms.TextBox();
+            this.txtFolderURL = new System.Windows.Forms.TextBox();
+            this.lstvRecordFile = new System.Windows.Forms.ListView();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnFolderURL = new System.Windows.Forms.Button();
+            this.btnPackageURL = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPackageURL
+            // 
+            this.txtPackageURL.Location = new System.Drawing.Point(119, 16);
+            this.txtPackageURL.Name = "txtPackageURL";
+            this.txtPackageURL.Size = new System.Drawing.Size(407, 20);
+            this.txtPackageURL.TabIndex = 0;
+            // 
+            // txtFolderURL
+            // 
+            this.txtFolderURL.Location = new System.Drawing.Point(119, 59);
+            this.txtFolderURL.Name = "txtFolderURL";
+            this.txtFolderURL.Size = new System.Drawing.Size(407, 20);
+            this.txtFolderURL.TabIndex = 1;
+            // 
+            // lstvRecordFile
+            // 
+            this.lstvRecordFile.Location = new System.Drawing.Point(119, 104);
+            this.lstvRecordFile.Name = "lstvRecordFile";
+            this.lstvRecordFile.Size = new System.Drawing.Size(407, 97);
+            this.lstvRecordFile.TabIndex = 2;
+            this.lstvRecordFile.UseCompatibleStateImageBehavior = false;
+            this.lstvRecordFile.View = System.Windows.Forms.View.List;
+            // 
+            // btnImport
+            // 
+            this.btnImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImport.Location = new System.Drawing.Point(558, 104);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(75, 97);
+            this.btnImport.TabIndex = 3;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Package URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Import Folder URL";
+            // 
+            // btnFolderURL
+            // 
+            this.btnFolderURL.Location = new System.Drawing.Point(558, 57);
+            this.btnFolderURL.Name = "btnFolderURL";
+            this.btnFolderURL.Size = new System.Drawing.Size(75, 23);
+            this.btnFolderURL.TabIndex = 6;
+            this.btnFolderURL.Text = "Choose";
+            this.btnFolderURL.UseVisualStyleBackColor = true;
+            this.btnFolderURL.Click += new System.EventHandler(this.btnFolderURL_Click);
+            // 
+            // btnPackageURL
+            // 
+            this.btnPackageURL.Location = new System.Drawing.Point(558, 14);
+            this.btnPackageURL.Name = "btnPackageURL";
+            this.btnPackageURL.Size = new System.Drawing.Size(75, 23);
+            this.btnPackageURL.TabIndex = 7;
+            this.btnPackageURL.Text = "Choose";
+            this.btnPackageURL.UseVisualStyleBackColor = true;
+            this.btnPackageURL.Click += new System.EventHandler(this.btnPackageURL_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Record File";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Green;
+            this.lblInfo.Location = new System.Drawing.Point(116, 215);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(34, 13);
+            this.lblInfo.TabIndex = 9;
+            this.lblInfo.Text = "*Info";
+            this.lblInfo.Visible = false;
+            // 
+            // ImportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 246);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnPackageURL);
+            this.Controls.Add(this.btnFolderURL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.lstvRecordFile);
+            this.Controls.Add(this.txtFolderURL);
+            this.Controls.Add(this.txtPackageURL);
+            this.Name = "ImportForm";
+            this.Text = "Import Record";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPackageURL;
+        private System.Windows.Forms.TextBox txtFolderURL;
+        private System.Windows.Forms.ListView lstvRecordFile;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnFolderURL;
+        private System.Windows.Forms.Button btnPackageURL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ Bài tập tuần #5 - SSIS/Thu_1542275/ImportApplication/ImportApplication/ImportForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Dts.Runtime;
+
+namespace ImportApplication
+{
+    public partial class ImportForm : Form
+    {
+        public ImportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPackageURL_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog choosefile = new OpenFileDialog();
+            choosefile.Filter = "dtsx|*.dtsx";
+            choosefile.FilterIndex = 1;
+            choosefile.Multiselect = true;
+
+            if (choosefile.ShowDialog() == DialogResult.OK)
+            {
+                txtPackageURL.Text = choosefile.FileName;
+            }
+        }
+
+        private void btnFolderURL_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.Description = "Chọn folder chứa tập tin import dạng .txt";
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderURL.Text = folderBrowser.SelectedPath;
+
+                DirectoryInfo dir = new DirectoryInfo(txtFolderURL.Text);
+                FileInfo[] TXTFiles = dir.GetFiles("*.txt");
+                if (TXTFiles.Length == 0)
+                {
+                    lstvRecordFile.Items.Clear();
+                }
+                foreach (var file in TXTFiles)
+                {
+                    if (file.Exists)
+                    {
+                        lstvRecordFile.Items.Add(file.Name);
+                    }
+                }
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            if (txtFolderURL.Text == "")
+            {
+                MessageBox.Show("Chưa chọn thư mục chứa file muốn import!", "Thông báo");
+            }
+            else if (txtPackageURL.Text == "")
+            {
+                MessageBox.Show("Chưa chọn package muốn gọi thực hiện import!", "Thông báo");
+            }
+            else if (lstvRecordFile.Items.Count == 0)
+            {
+                MessageBox.Show("Thư mục chọn không có file để import!", "Thông báo");
+            }
+            else
+            {
+                lblInfo.Visible = true;
+                lblInfo.Text = "Tiến hành import toàn bộ file trong thư mục " + txtFolderURL.Text;
+
+                string pkgLocation = txtPackageURL.Text.Replace(@"\", "\\");;
+
+                Package pkg;
+                Microsoft.SqlServer.Dts.Runtime.Application app;
+                DTSExecResult pkgResults;
+                Variables vars;
+
+                app = new Microsoft.SqlServer.Dts.Runtime.Application();
+                pkg = app.LoadPackage(pkgLocation, null);
+
+                vars = pkg.Variables;
+                foreach (ListViewItem file in lstvRecordFile.Items)
+                {
+                     string fileURL = txtFolderURL.Text + @"\" + file.Text;
+                     vars["file_path_import"].Value = fileURL.Replace(@"\", "\\"); ;
+
+                     pkgResults = pkg.Execute(null, vars, null, null, null);
+
+                     if (pkgResults == DTSExecResult.Success)
+                            lblInfo.Text = "Package ran successfully on " + file.Text;
+                      else
+                            lblInfo.Text = "Package failed on " + file.Text;
+                }
+
+                lblInfo.Text = "Processing completed!";
+            }
+        }
+    }
+}
