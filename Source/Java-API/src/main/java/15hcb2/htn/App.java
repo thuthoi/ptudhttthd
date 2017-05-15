ed283aa1a2c701fbac9365a7edc659c25f1b0b6d
htnhan93 <htnhan93@gmail.com>
2017-05-15 22:37:42 +0700

    Tạo project java
    Tạo 2 project java:
    1. Java Backend: xử lý kết nối CSDL
    2. Java Api: xử lý các API

---
+++ Source/Java-API/src/main/java/15hcb2/htn/App.java
@@ -0,0 +1,13 @@
+package 15hcb2.htn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Source/Java-Back-End/src/main/java/15hcb2/hnt/App.java
@@ -0,0 +1,13 @@
+package 15hcb2.hnt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
